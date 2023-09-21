0e8d9b05a54447b7c4f160f7855a05e512fb1e0d
StanislavJ4Y <115622736+StanislavJ4Y@users.noreply.github.com>
2023-09-21 18:11:03 +0300

    Add files via upload

---
+++ week 3/20s.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cout << "Enter a three-digit number: ";
+    cin >> N;
+
+    if (N < 100 || N > 999) {
+        cout << "The entered number is not three-digit.\n";
+        return 1;
+    }
+
+    int digit1 = N / 100;
+    int digit2 = (N % 100) / 10;
+    int digit3 = N % 10;
+
+
+    int calculate = 0;
+
+        if (digit1 % 2 == 0) {
+        calculate++;
+    }
+
+    if (digit2 % 2 == 0) {
+        calculate++;
+    }
+
+    if (digit3 % 2 == 0) {
+        calculate++;
+    }
+
+    cout << "Decomposition of the number " << N << " into digits: " << digit1 << ", " << digit2 << ", " << digit3 << endl;
+    cout << "Number of even digits: " << calculate << endl;
+
+    return 0;
+}
---
+++ week 3/6s.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cout << "Enter a three-digit number: ";
+    cin >> N;
+
+    if (N < 100 || N > 999) {
+        cout << "The entered number is not three-digit.\n";
+        return 1;
+    }
+
+    int digit1 = N / 100;
+    int digit2 = (N % 100) / 10;
+    int digit3 = N % 10;
+
+    int calculate = 0;
+
+    if (digit1 > 3) {
+        calculate++;
+    }
+
+    if (digit2 > 3) {
+        calculate++;
+    }
+
+    if (digit3 > 3) {
+        calculate++;
+    }
+
+    cout << "Decomposition of the number " << N << " into digits: " << digit1 << ", " << digit2 << ", " << digit3 << endl;
+    cout << "Number of digits greater than 3: " << calculate << endl;
+
+    return 0;
+}
