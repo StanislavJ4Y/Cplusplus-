2264c0589d0155848526c0af90f95d4a7ddda32a
StanislavJ4Y <115622736+StanislavJ4Y@users.noreply.github.com>
2023-11-26 16:04:43 +0200

    Update second task.cpp

--- week 9/second task.cpp
+++ week 9/second task.cpp
@@ -6,26 +6,31 @@ int main() {
     int arr[size];
 
     cout << "Enter " << size << " integers:" << endl;
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
 
     int maxElement = arr[0];
+    int maxIndex = 0;
 
-    for(int i = 1; i < size; i++){
-        if(arr[i] > maxElement) {
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxElement) {
             maxElement = arr[i];
+            maxIndex = i;
         }
     }
 
-    for(int i = 0; i < size; i++){
-        if(arr[i] < 0 && i < maxElement) {
-            arr[i] = 0;
-        }
+
+    for (int i = maxIndex + 1; i < size; i++) {
+        if (arr[i] < 0) {
+        arr[i] = 0;
+    } else {
+        break;
     }
+}
 
     cout << "Updated array:" << endl;
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
