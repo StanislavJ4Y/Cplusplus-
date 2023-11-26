8646e855bf9e890bd9d2c2059a9ca7e0191187d4
StanislavJ4Y <115622736+StanislavJ4Y@users.noreply.github.com>
2023-11-26 15:47:45 +0200

    first task.cpp

--- week 9/first task.cpp
+++ week 9/first task.cpp
@@ -2,52 +2,45 @@
 #include <cstdlib>
 using namespace std;
 
-void insertionSort(int arr[], int size);
-void printArray(int arr[], int size);
-
 int main() {
-    const int SIZE = 13;
+    const int SIZE = 12;
     int arr[SIZE];
 
     srand(time(0));
 
     for (int i = 0; i < SIZE; i++) {
-        arr[i] = rand() % 100; 
+        arr[i] = rand() % 100;
     }
 
     cout << "Array before sorting: ";
-    printArray(arr, SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
-    insertionSort(arr, SIZE);
+
+    for (int i = 1; i < SIZE; i++) {
+        for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+            swap(arr[j], arr[j - 1]);
+        }
+    }
 
     cout << "\nArray in ascending order: ";
-    printArray(arr, SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
 
     for (int i = 0; i < SIZE / 2; i++) {
         swap(arr[i], arr[SIZE - i - 1]);
     }
 
     cout << "\nArray in descending order: ";
-    printArray(arr, SIZE);
-
-    return 0;
-}
-
-void insertionSort(int arr[], int size) {
-    for (int i = 1; i < size; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < SIZE; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
+
+    return 0;
 }
