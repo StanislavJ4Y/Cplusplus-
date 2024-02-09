38e882816c927a2e2f6595909b5788db72368549
StanislavJ4Y <115622736+StanislavJ4Y@users.noreply.github.com>
2024-02-09 15:22:50 +0200

    Add files via upload

---
+++ 2 семестр/week 1(semester 2)/week 1(semester 2)/firs task.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+
+    int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
+    int n = sizeof(vector) / sizeof(vector[0]);
+
+    int *ptr = vector;
+    int *minPtr = ptr;
+
+    for (int i = 0; i < n; i++) {
+        if (*minPtr > *(ptr + i)) {
+            minPtr = ptr + i;
+        }
+    }
+        cout << "Minumum element is: " << *minPtr << " in our array." << endl;
+
+    return 0;
+}
